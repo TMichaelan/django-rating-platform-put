72766ae481ebeeb9d72dcc65d9a56e1906184d31
TMichael <bigapplematthias@gmail.com>
2023-05-22 12:54:15 +0200

    update admin.py

--- movielens/userview/admin.py
+++ movielens/userview/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Rating, Genre, Movie
+from .models import Rating, Genre, Movie,Comment,ImdbRating
 
 admin.site.register(Genre)
 admin.site.register(Movie)
 admin.site.register(Rating)
+admin.site.register(Comment)
+admin.site.register(ImdbRating)
